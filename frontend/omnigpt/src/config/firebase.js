89991084ce5ec53364e89645b7ca4445029639af
Parth Rajendra Karkhanis <pr401159@dal.ca>
2024-06-14 15:46:36 -0300

    Firebase config added

---
+++ frontend/omnigpt/src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCYjC3rg8FPVwDiU6kfDCTCbNRqheRVebI",
+  authDomain: "omnigpt-bf0f1.firebaseapp.com",
+  projectId: "omnigpt-bf0f1",
+  storageBucket: "omnigpt-bf0f1.appspot.com",
+  messagingSenderId: "194161797202",
+  appId: "1:194161797202:web:fd9b384cd56403eeac578b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
