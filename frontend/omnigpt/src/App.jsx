b3d723d7e7145244184a3cedd00cfd3d217a4e2e
Aniket Mhatre <an370985@dal.ca>
2024-06-14 10:20:20 -0300

    Landing page created for the OmniGPT

--- frontend/omnigpt/src/App.jsx
+++ frontend/omnigpt/src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Landing from './pages/Landing';
 function App() {
-
-  return (
-    <>
-      <h1 className='text-3xl font-bold'>OmniGPT</h1>
-    </>
-  )
+    return (
+      <Router>
+          <Routes>
+            {/* Accessible Routes */}
+            <Route path='/' element={<Landing />}></Route>
+          </Routes>
+      </Router>
+    )
 }
 
 export default App
