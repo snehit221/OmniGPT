53acccafab5453b6bcce19f16514d4b5771a983d
Shubham Bhushan Pawar <sh764760@dal.ca>
2024-06-14 22:06:52 -0300

    added pricing page

--- frontend/omnigpt/src/App.jsx
+++ frontend/omnigpt/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Register from './pages/Register';
+import Pricing from './pages/Pricing';
 
 function App() {
     return (
@@ -9,6 +10,8 @@ function App() {
             {/* Accessible Routes */}
             <Route path='/' element={<Landing />}></Route>
             <Route path='/register' element={<Register />}></Route>
+            <Route path='/pricing' element={<Pricing />}></Route>
+
           </Routes>
       </Router>
     )
